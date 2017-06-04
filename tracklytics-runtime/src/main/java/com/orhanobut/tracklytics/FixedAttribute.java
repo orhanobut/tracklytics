1473abd1704d89a74fabb2daf2ad88572334e78d
Orhan Obut <orhan.obut@blacklane.com>
2017-06-04 11:01:34 +0200

    Update Travis for build-tools

--- tracklytics-runtime/src/main/java/com/orhanobut/tracklytics/FixedAttribute.java
+++ tracklytics-runtime/src/main/java/com/orhanobut/tracklytics/FixedAttribute.java
@@ -7,7 +7,6 @@ import java.lang.annotation.RetentionPolicy;
  * Assign a fixed/static value for the given attribute
  * This can be used in the situations that the actual value is not dynamic.
  * For example: A good case would be screen name or button name
- * 
  * <p>
  * <pre>
  *   <code>{@literal @}FixedAttribute(key="scree_name", value="Login")
